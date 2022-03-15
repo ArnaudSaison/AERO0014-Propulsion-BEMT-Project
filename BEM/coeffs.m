6ac29e2a1e104b8059c35000450d9dc923f5e44b
ArnaudSaison <44066592+ArnaudSaison@users.noreply.github.com>
2022-03-15 17:04:32 +0100

    integrate instead of intPop

--- BEM/coeffs.m
+++ BEM/coeffs.m
@@ -7,6 +7,6 @@ function [C_T, C_P, eta_P, J] = coeffs(res, p)
 C_T = 4 * res.T / (p.D^2 * p.rho * p.Omega_n^2 * p.D^2);     % thrust coefficient
 C_P = 4 * res.P / (p.rho * p.Omega_n^3 * p.D^5);             % power coefficient
 eta_P = res.T * p.v_inf / res.P;                                    % propulsive efficiency
-J = p.v_inf / (p.Omega / (2*pi) * p.D);                             % advance ratio
+J = p.v_inf / (p.Omega_n * p.D);                             % advance ratio
 
 end
